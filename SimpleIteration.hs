44f6ba492d78d0cb19afbc1666f1128e262bfb10
Evgeny Vinogradov <zhenyavinogradov@gmail.com>
2015-12-02 11:17:59 +0300

    Implemented simple iteration visualization

--- SimpleIteration.hs
+++ SimpleIteration.hs
@@ -1,16 +1,53 @@
-{-# LANGUAGE FlexibleContexts, UnicodeSyntax #-}
+{-# LANGUAGE UnicodeSyntax #-}
+module SimpleIterations where
 
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
 
-logmapfoo :: Double → Double → Double
-logmapfoo r x = r * x * (1 - x)
-logmap  = iterate . logmapfoo
+r, x₀ :: Double
+r  = 2.8
+x₀ = 0.5
 
-main = toFile def "example1_big.png" $ do
-    layout_title .= "Amplitude Modulation"
-    setColors [opaque blue, opaque pink, opaque red, opaque green,  opaque maroon]
-    let rs = [0.5, 1.5, 2.5, 3.1, 3.44]
-    mapM_ (\rs → plot (line
-                       ("Convergence: " ++ show rs)
-                       [take 50 $ zip [(0 :: Double)..] $ logmap rs 0.5])) rs
+φ :: Double → Double
+φ x = r * x * (1 - x)
+
+iter = iterate φ x₀
+
+snce = plot (line "" [take 50 $ zip [(0::Int)..] iter])
+
+
+range = [0,0.05..1]
+plt :: (Double → Double) → [(Double, Double)]
+plt f = map (\x → (x, f x)) range
+
+-- x₀,0 → x₀,x₁ → x₁,x₁ → x₁,x₂ → x₂,x₂ → …
+-- x,y → x,φx → φx,φx → …
+visup = next (x₀,0) where 
+  next (x, y) = (x, y) : (x, φ x) : next (φ x, φ x)
+
+visu = do
+  plot (line "" [plt φ])
+  plot (line "" [plt id])
+  plot (line "" [take 50 visup])
+
+
+φr :: Double → Double → Double
+φr r x = r * x * (1 - x)
+
+pres = 500
+post = 100
+
+atrs :: Double → [Double]
+atrs r = take post $ drop pres $ iterate (φr r) 0.5
+
+bifup :: [(Double, Double)]
+bifup = concatMap (\x → map ((,) x) (atrs x)) [(1.0::Double),1.005..4]
+
+bifu = do
+  setColors [black `withOpacity` 0.2]
+  plot (points "" bifup)
+
+main = do
+  toFile def "1a-snce.png" snce
+  toFile def "1b-visu.png" visu
+  toFile def "1c-bifu.png" bifu
