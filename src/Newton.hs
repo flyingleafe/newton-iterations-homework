727f9af272f56e038a753652419ce6a1d498b6dd
Evgeny Vinogradov <zhenyavinogradov@gmail.com>
2015-12-03 13:17:35 +0300

    Added getting color by pixel number

--- src/Newton.hs
+++ src/Newton.hs
@@ -14,24 +14,23 @@ type Cex = Complex Double
 φ  z = z**3 - 1
 φ' z = 3 * z**2
 
-next :: Cex → Cex
 next z = z - φ z / φ' z
-
-iter :: Cex → [Cex]
 iter = iterate next
 
+     {-
 start = -2
 end = 2
-step = 0.005
+step = 0.05
 
 alp :: [Cex]
 alp = [ a :+ b | a ← l, b ← l ]
     where l = [start,start+step..end]
+    -}
 
 roots :: [Cex]
 roots = [1:+0, mkPolar 1 (2*pi/3), mkPolar 1 (4*pi/3)]
       
-eps = 0.0001
+--eps = 0.0001
 nearTo :: Cex → Maybe Int
 nearTo z | z==0 = Just 3 -- magnitude z < eps = Just 3
 nearTo z = findIndex isNear roots where
@@ -44,6 +43,7 @@ takeFirst l f = head $ mapMaybe f l
 cls :: Cex → Int
 cls z = takeFirst (iter z) nearTo
 
+    {-
 toPair :: Cex → (Double, Double)
 toPair (a :+ b) = (a, b)
     
@@ -57,6 +57,7 @@ points' values = liftEC $ do
     plot_points_style . point_shape .= shape
     plot_points_style . point_radius .= 1
 
+newt :: EC (Layout Double Double) ()
 newt = do
   layout_x_axis . laxis_generate .= scaledAxis def (start,end)
   layout_y_axis . laxis_generate .= scaledAxis def (start,end)
@@ -64,7 +65,21 @@ newt = do
   setShapes [PointShapePolygon 4 False]
   flip mapM_ [0,1,2,3]
     $ \i → plot (points' $ map toPair $ filter ((==i) . cls) alp)
+  -}
 
+newtonPicSize = 1000
+range = 2
+
+getNewtonColor :: (Int, Int) → Int
+getNewtonColor (a,b) = cls z where
+  z = za :+ zb
+  za = norm a
+  zb = norm b
+  norm :: Int → Double
+  norm s = (2 * fromIntegral s / fromIntegral newtonPicSize - 1) * fromIntegral range
+
+  {-
 main = do
   let a = toRenderable $ execEC newt
   renderableToFile (FileOptions (800, 800) PNG) "2-newt.png" a
+  -}
